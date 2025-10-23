bc5e5fcca449353a49efeb2a6dfa4a9b466e801e
medfarouk <med.faroukakacha@gmail.com>
2025-10-24 00:33:50 +0100

    - implement appwrite services with the latest version - update search view for upserting the movies to the database

---
+++ services/appwrite.ts
@@ -0,0 +1,46 @@
+// track the search made by user
+
+import {Client, ID, Query, TablesDB} from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const TABLE_ID = process.env.EXPO_PUBLIC_APPWRITE_TABLE_ID!;
+
+const client = new Client()
+    .setEndpoint(`${process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT}`)
+    .setProject(`${process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID}`)
+    .setPlatform(`${process.env.EXPO_PUBLIC_APPWRITE_PLATFORM}`);
+
+const dataBase = new TablesDB(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+    try {
+        const result = await dataBase.listRows({
+            databaseId: DATABASE_ID,
+            tableId: TABLE_ID,
+            queries: [
+                Query.equal('searchTerm', query)
+            ]
+        });
+
+        const existingMovie = result.rows[0];
+        await dataBase.upsertRow({
+            databaseId: DATABASE_ID,
+            tableId: TABLE_ID,
+            rowId: existingMovie ? existingMovie.$id : ID.unique(),
+            data: existingMovie ?
+                {
+                    ...existingMovie,
+                    count: existingMovie.count + 1
+                } : {
+                    searchTerm: query,
+                    movie_id: movie.id,
+                    movie_title: movie.title,
+                    poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                    count: 1
+                }
+        });
+    } catch (error) {
+        console.error("Error updating search count:", error);
+        throw error;
+    }
+}
